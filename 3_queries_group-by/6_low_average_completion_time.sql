d95d58679b1817aa37d643c350ae09d65abc2171
trevorfunk <trevorjonfunk@gmail.com>
2022-08-02 15:55:43 -0700

    add queries for group by assignments

---
+++ 3_queries_group-by/6_low_average_completion_time.sql
@@ -0,0 +1,8 @@
+SELECT students.name AS student, avg(assignment_submissions.duration) AS average_assignment_duration, avg(assignments.duration) AS average_estimated_duration
+FROM students
+JOIN assignment_submissions ON student_id = students.id
+JOIN assignments on assignment_id = assignments.id
+WHERE students.end_date IS NULL
+GROUP BY students.name
+HAVING avg(assignment_submissions.duration) < avg(assignments.duration)
+ORDER BY average_assignment_duration ASC
